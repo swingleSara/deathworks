c4e300995eea16bf4407a281d41c1cb815e19e03
Sara Swingle <sara.swingle.dev@gmail.com>
2022-10-10 11:43:08 -0600

    Update profile with seeker and employer customization

--- controllers/profile.js
+++ controllers/profile.js
@@ -8,14 +8,18 @@ module.exports = {
   getProfile: async (req, res) => {
     try {
       const listings = await Listing.find({ user: req.user.id });
+      const questions = await Question.find({ user: req.user.id }).sort({ createdAt: "desc"}).lean();
       const employer = await Employer.find({ user: req.user.id });
       const seeker = await Seeker.find({ user: req.user.id });
+      const allListings = await Listing.find().sort({ createdAt: "desc"}).lean();
       res.render("profile.ejs", {
         listings: listings,
+        questions: questions,
         user: req.user,
         status: req.user.status,
         employer: employer,
         seeker: seeker,
+        allListings: allListings
       });
     } catch (err) {
       console.log(err);
